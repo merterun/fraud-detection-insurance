26492e65fd0f43a8d62e6910932ceeb6b472df7d
samwise <44654977+merterun@users.noreply.github.com>
2023-05-01 20:13:33 +0300

    Add files via upload

--- steps/fraud_feature_importance.R
+++ steps/fraud_feature_importance.R
@@ -1,15 +1,18 @@
 train$FraudFound_P <- as.numeric(train$FraudFound_P)
+train$FraudFound_P[train$FraudFound_P == 1] <- 0
+train$FraudFound_P[train$FraudFound_P == 2] <- 1
 
+unique(train$FraudFound_P)
 
 # Train an XGBoost model with the optimal hyperparameters
 xgb_modelv3 <- xgboost(
   data = as.matrix(train[, -which(names(train) == "FraudFound_P")]), # exclude target variable
   label = train$FraudFound_P,
-  nrounds = 79,
-  eta = 0.115,
-  max_depth = 10,
-  subsample = 0.939,
-  colsample_bytree = 0.785,
+  nrounds = 80,
+  eta = 0.157,
+  max_depth = 5,
+  subsample = 0.863,
+  colsample_bytree = 0.768,
   objective = "binary:logistic"
 )
 
@@ -20,7 +23,6 @@ summary(xgb_modelv3)
 importance_matrix <- xgb.importance(names(train[, -1]), model = xgb_modelv3)
 summary(importance_matrix)
 str(importance_matrix)
-importance_matrix
 
 
 xgb.plot.importance(importance_matrix)
